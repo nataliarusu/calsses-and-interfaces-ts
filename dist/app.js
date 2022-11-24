8313b733e201c3d80c38c11e58f22090e0fac7a1
Natalia Rusu <bonapartik@gmail.com>
2022-11-24 11:42:39 +0000

    optional parametres and properties in classes and interfaces

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,11 @@
 "use strict";
-//every object that is instance of class Person should have Greetable properties
+//every object that is instance of class Person should have Greetable properties, and Greetable inforces to have Named properties
 class Person {
     constructor(n, age) {
         this.name = n;
-        this.age = age;
+        if (age) {
+            this.age = age;
+        }
     }
     greet(phrase) {
         console.log(phrase + ' ' + this.name);
@@ -12,10 +14,17 @@ class Person {
         console.log(this.name);
     }
     printAge() {
-        console.log(this.age);
+        if (this.age) {
+            console.log(this.age);
+        }
+        else {
+            console.log('Hi');
+        }
     }
 }
-let person2; //we later create an object based on interface
-person2 = new Person('Max', 30);
-person2.greet('Hi there, I am');
+let person1; //we later create an object based on interface
+person1 = new Person('Max', 30);
+person1.greet('Hi there, I am');
+const person2 = new Person('Manu');
+person2.printAge();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 interface Named {
   readonly name: string; //force to initialize once
+  printName?:() => void; //? means optional, classes that implements Named can have or not have printName()
 }
 interface Greetable extends Named {
   //intefaces can exptends from multiple interfaces   //interface Greetable extends Named, AnotherOne
@@ -10,10 +11,12 @@ interface Greetable extends Named {
 class Person implements Greetable {
   //class Person implements Greetable, Named{} also possible if Greetable doesn't extends from Named
   public name: string;
-  public age: number;
-  constructor(n: string, age: number) {
+  public age?: number;
+  constructor(n: string, age?: number) {
     this.name = n;
-    this.age = age;
+    if(age){
+        this.age = age;
+    } 
   }
   greet(phrase: string): void {
     console.log(phrase + ' ' + this.name);
@@ -22,12 +25,17 @@ class Person implements Greetable {
     console.log(this.name);
   }
   printAge() {
-    console.log(this.age);
+    if(this.age){
+        console.log(this.age);
+    } else {
+        console.log('Hi')
+    }
+   
   }
 }
 
-let person2: Greetable; //we later create an object based on interface
-
-person2 = new Person('Max', 30);
-
-person2.greet('Hi there, I am');
+let person1: Greetable; //we later create an object based on interface
+person1 = new Person('Max', 30);
+person1.greet('Hi there, I am');
+const person2: Person = new Person('Manu');
+person2.printAge();
